c033ab42472cdace5007eb89261d1f0a65274f24
pierre <pierreladen@hotmail.fr>
2019-05-10 10:53:41 +0200

    [ADD] route for get last message for all conversation for a user

--- chatbot-msg.js
+++ chatbot-msg.js
@@ -35,37 +35,80 @@ app.get('/conversation/:uid', function (req, res) {
     })
 });
 
+app.post('/conversation/all', function (req, res) {
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+        var query = {
+            'token': req.body.token
+        };
+        var arr = [];
+        db.collection('users').findOne(query, function (err, result) {
+            for (let i = 0; i < result.conversations.length; i++) {
+                arr.push({ "conversation.uid" :result.conversations[i] }) ;
+            }
+            db.collection('chatbot').aggregate([{
+                $project: {
+                    "_id" : 0,
+                    "conversation.messages": [{
+                        $arrayElemAt: [ "$conversation.messages", -1 ]
+                    }],
+                    "members" : 1,
+                    'conversation.uid': 1
+                }
+            },            {
+            $match: {
+                $or: arr
+            } }
+            ]).toArray(function(err, result) {
+                res.send(JSON.stringify(result));
+                client.close();
+            });
+        })
+    });
+});
+
+app.get('/conversation/last/:uid', function (req, res) {
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+        db.collection('chatbot').aggregate([{
+            $project: {
+                "_id" : 0,
+                "conversation.messages": [{
+                    $arrayElemAt: [ "$conversation.messages", -1 ]
+                }],
+                "members" : 1,
+                'conversation.uid': 1 }
+                },            {
+                $match: {
+                    "conversation.uid" : req.params.uid
+                } }
+
+        ]).toArray(function(err, result) {
+            res.send(JSON.stringify(result[0]));
+            client.close();
+        });
+    })
+});
+
 app.delete('/conversation/', function (req, res) {
     console.log(req);
     MongoClient.connect(url, function (err, client) {
         const db = client.db(dbName);
         var query = {
-            'conversation.uid': req.body.uid
-        };
-        db.collection('chatbot').findOneAndDelete(query, function(err, result) {
-            if (result.value != null) {
-                res.send(JSON.stringify({"state": "success"}));
-
-            } else {
-                res.send(JSON.stringify({"state" : "error", "message" : "bad uid"}));
-            }
-            client.close();
-        });
-        var query2 = {
             token: req.body.token
         };
-        db.collection('users').findOneAndUpdate(query2, {$pull: {"conversations" : req.body.uid}}, function (err, result) {
+        db.collection('users').findOneAndUpdate(query, {$pull: {"conversations" : req.body.uid}}, function (err, result) {
+            res.send(JSON.stringify({"state": "success"}));
             client.close();
         });
-        db.collection('doctors').findOneAndUpdate(query2, {$pull: {"conversations" : req.body.uid}}, function (err, result) {
+        db.collection('doctors').findOneAndUpdate(query, {$pull: {"conversations" : req.body.uid}}, function (err, result) {
+            res.send(JSON.stringify({"state": "success"}));
             client.close();
         });
     });
 });
-
-function addConversationToUser($user_id) {
-
-}
 
 app.post('/conversation', function (req, res) {
     MongoClient.connect(url, function (err, client) {
@@ -126,7 +169,7 @@ app.post('/message', function (req, res) {
                 'conversation.uid' : req.body.uid
             };
             res.setHeader('Content-Type', 'application/json; charset=UTF-8');
-            if (req.body.member !== "000000000000000000000001") {
+            if (req.body.message.member !== "000000000000000000000001") {
                 var message = {
                     "type" : req.body.message.type,
                     "member" : ObjectId(req.body.message.member),
