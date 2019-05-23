d5fec51a18dc09be2c1ecfa3333778e3e7385914
pierre <pierreladen@hotmail.fr>
2019-05-23 10:57:16 +0200

    up

--- chatbot-msg.js
+++ chatbot-msg.js
@@ -44,25 +44,30 @@ app.post('/conversation/all/user', function (req, res) {
         };
         var arr = [];
         db.collection('users').findOne(query, function (err, result) {
-            for (let i = 0; i < result.conversations.length; i++) {
-                arr.push({ "conversation.uid" : result.conversations[i] }) ;
-            }
-            db.collection('chatbot').aggregate([{
-                $project: {
-                    "_id" : 0,
-                    "conversation.messages": [{
-                        $arrayElemAt: [ "$conversation.messages", -1 ]
-                    }],
-                    "members" : 1,
-                    'conversation.uid': 1
+            if (result != null) {
+                for (let i = 0; i < result.conversations.length; i++) {
+                    arr.push({ "conversation.uid" : result.conversations[i] }) ;
                 }
-            },            {
-            $match: {
-                $or: arr
-            } }
-            ]).toArray(function(err, result) {
-                res.send(JSON.stringify({"state" : "success", "lastMessages" : result}));
-            });
+                db.collection('chatbot').aggregate([{
+                    $project: {
+                        "_id" : 0,
+                        "conversation.messages": [{
+                            $arrayElemAt: [ "$conversation.messages", -1 ]
+                        }],
+                        "members" : 1,
+                        'conversation.uid': 1
+                    }
+                },            {
+                    $match: {
+                        $or: arr
+                    } }
+                ]).toArray(function(err, result) {
+                    res.send(JSON.stringify({"state" : "success", "lastMessages" : result}));
+                });
+                
+            } else {
+                res.send(JSON.stringify({"state" : "error", "message" : "bad token"}));
+            }
             client.close();
         });
     });
@@ -78,25 +83,31 @@ app.post('/conversation/all/doctor', function (req, res) {
         var arr = [];
         db.collection('doctors').findOne(query, function (err, result) {
             console.log(result);
-            for (let i = 0; i < result.conversations.length; i++) {
-                arr.push({ "conversation.uid" : result.conversations[i] }) ;
-            }
-            db.collection('chatbot').aggregate([{
-                $project: {
-                    "_id" : 0,
-                    "conversation.messages": [{
-                        $arrayElemAt: [ "$conversation.messages", -1 ]
-                    }],
-                    "members" : 1,
-                    'conversation.uid': 1
+            if (result != null) {
+
+                for (let i = 0; i < result.conversations.length; i++) {
+                    arr.push({"conversation.uid": result.conversations[i]});
                 }
-            },            {
-                $match: {
-                    $or: arr
-                } }
-            ]).toArray(function(err, result) {
-                res.send(JSON.stringify({"state" : "success", "lastMessages" : result}));
-            });
+                db.collection('chatbot').aggregate([{
+                    $project: {
+                        "_id": 0,
+                        "conversation.messages": [{
+                            $arrayElemAt: ["$conversation.messages", -1]
+                        }],
+                        "members": 1,
+                        'conversation.uid': 1
+                    }
+                }, {
+                    $match: {
+                        $or: arr
+                    }
+                }
+                ]).toArray(function (err, result) {
+                    res.send(JSON.stringify({"state": "success", "lastMessages": result}));
+                });
+            } else {
+                res.send(JSON.stringify({"state" : "error", "message" : "bad token"}));
+            }
             client.close();
         })
     });
