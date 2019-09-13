60a1c1d07b7b8952836b2036d765aae57d1d10fb
pierre <pierreladen@hotmail.fr>
2019-09-13 10:28:21 +0200

    notif ios

--- chatbot-msg.js
+++ chatbot-msg.js
@@ -244,89 +244,97 @@ app.post('/message', function (req, res) {
                     "read" : true
                 };
             }
-
-            var query2 = {
-                user_id : ObjectId(req.body.message.member)
-            };
-            db.collection('users_token').find(query2).toArray(function(err, result) {
-                if (result) {
-                    const data = {
-                        title: 'New push notification', // REQUIRED for Android
-                        topic: 'topic', // REQUIRED for iOS (apn and gcm)
-                        body: 'Powered by AppFeel',
-                        custom: {
-                            sender: 'AppFeel',
-                        },
-                        priority: 'high', // gcm, apn. Supported values are 'high' or 'normal' (gcm). Will be translated to 10 and 5 for apn. Defaults to 'high'
-                        collapseKey: '', // gcm for android, used as collapseId in apn
-                        contentAvailable: true, // gcm, apn. node-apn will translate true to 1 as required by apn.
-                        delayWhileIdle: true, // gcm for android
-                        restrictedPackageName: '', // gcm for android
-                        dryRun: false, // gcm for android
-                        icon: '', // gcm for android
-                        image: '', // gcm for android
-                        style: '', // gcm for android
-                        picture: '', // gcm for android
-                        tag: '', // gcm for android
-                        color: '', // gcm for android
-                        clickAction: '', // gcm for android. In ios, category will be used if not supplied
-                        locKey: '', // gcm, apn
-                        locArgs: '', // gcm, apn
-                        titleLocKey: '', // gcm, apn
-                        titleLocArgs: '', // gcm, apn
-                        retries: 1, // gcm, apn
-                        encoding: '', // apn
-                        badge: 2, // gcm for ios, apn
-                        sound: 'ping.aiff', // gcm, apn
-                        android_channel_id: '', // gcm - Android Channel ID
-                        alert: { // apn, will take precedence over title and body
-                            title: 'title',
-                            body: 'body'
-                            // details: https://github.com/node-apn/node-apn/blob/master/doc/notification.markdown#convenience-setters
-                        },
-                        /*
-                         * A string is also accepted as a payload for alert
-                         * Your notification won't appear on ios if alert is empty object
-                         * If alert is an empty string the regular 'title' and 'body' will show in Notification
-                         */
-                        // alert: '',
-                        launchImage: '', // apn and gcm for ios
-                        action: '', // apn and gcm for ios
-                        category: '', // apn and gcm for ios
-                        // mdm: '', // apn and gcm for ios. Use this to send Mobile Device Management commands.
-                        // https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/MobileDeviceManagementProtocolRef/3-MDM_Protocol/MDM_Protocol.html
-                        urlArgs: '', // apn and gcm for ios
-                        truncateAtWordEnd: true, // apn and gcm for ios
-                        mutableContent: 0, // apn
-                        threadId: '', // apn
-                        // if both expiry and timeToLive are given, expiry will take precedence
-                        expiry: Math.floor(Date.now() / 1000) + 28 * 86400, // seconds
-                        timeToLive: 28 * 86400,
-                        headers: [], // wns
-                        launch: '', // wns
-                        duration: '', // wns
-                        consolidationKey: 'my notification', // ADM
-                    };
-                    const registrationIds = [];
-                    for (i = 0; i !== result.length ; i++) {
-                        registrationIds.push("42850e679b37b6ab0ad03e30cf9aa551c2a2ff45766d6f1d317daca337c72fae");
-                    }
-                    push.send(registrationIds, data, (err, result) => {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log(result);
-                        }
-                    });
-                }
-                client.close();
-            });
-
             var query = {
                 'conversation.uid' : req.body.uid
             };
-            db.collection('chatbot').findOneAndUpdate(query, {$push: {"conversation.messages" : message}}, function(err, result) {
+            db.collection('chatbot').findOneAndUpdate(query, {$push: {"conversation.messages" : message}}, {returnOriginal:false} ,function(err, result) {
                 if (result.value != null) {
+                    var query2 = null;
+                    if (result.value.members[0].toString() === req.body.message.member) {
+                         query2 = {
+                            user_id : ObjectId(result.value.members[1])
+                        };
+                    } else {
+                         query2 = {
+                            user_id : ObjectId(result.value.members[0])
+                        };
+                    }
+                    db.collection('users_token').find(query2).toArray(function(err, result) {
+                        if (result) {
+
+                            const data = {
+                                title: 'New push notification', // REQUIRED for Android
+                                topic: 'com.maximekillinger.apphomedoc', // REQUIRED for iOS (apn and gcm)
+                                body: 'Powered by AppFeel',
+                                custom: {
+                                    sender: 'AppFeel',
+                                },
+                                priority: 'high', // gcm, apn. Supported values are 'high' or 'normal' (gcm). Will be translated to 10 and 5 for apn. Defaults to 'high'
+                                collapseKey: '', // gcm for android, used as collapseId in apn
+                                contentAvailable: true, // gcm, apn. node-apn will translate true to 1 as required by apn.
+                                delayWhileIdle: true, // gcm for android
+                                restrictedPackageName: '', // gcm for android
+                                dryRun: false, // gcm for android
+                                icon: '', // gcm for android
+                                image: '', // gcm for android
+                                style: '', // gcm for android
+                                picture: '', // gcm for android
+                                tag: '', // gcm for android
+                                color: '', // gcm for android
+                                clickAction: '', // gcm for android. In ios, category will be used if not supplied
+                                locKey: '', // gcm, apn
+                                locArgs: '', // gcm, apn
+                                titleLocKey: '', // gcm, apn
+                                titleLocArgs: '', // gcm, apn
+                                retries: 1, // gcm, apn
+                                encoding: '', // apn
+                                badge: 2, // gcm for ios, apn
+                                sound: 'ping.aiff', // gcm, apn
+                                android_channel_id: '', // gcm - Android Channel ID
+                                alert: { // apn, will take precedence over title and body
+                                    title: 'Nouveau message sur Home’Doc',
+                                    body: req.body.message.text
+                                    // details: https://github.com/node-apn/node-apn/blob/master/doc/notification.markdown#convenience-setters
+                                },
+                                /*
+                                 * A string is also accepted as a payload for alert
+                                 * Your notification won't appear on ios if alert is empty object
+                                 * If alert is an empty string the regular 'title' and 'body' will show in Notification
+                                 */
+                                // alert: '',
+                                launchImage: '', // apn and gcm for ios
+                                action: '', // apn and gcm for ios
+                                category: '', // apn and gcm for ios
+                                // mdm: '', // apn and gcm for ios. Use this to send Mobile Device Management commands.
+                                // https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/MobileDeviceManagementProtocolRef/3-MDM_Protocol/MDM_Protocol.html
+                                urlArgs: '', // apn and gcm for ios
+                                truncateAtWordEnd: true, // apn and gcm for ios
+                                mutableContent: 0, // apn
+                                threadId: '', // apn
+                                // if both expiry and timeToLive are given, expiry will take precedence
+                                expiry: Math.floor(Date.now() / 1000) + 28 * 86400, // seconds
+                                timeToLive: 28 * 86400,
+                                headers: [], // wns
+                                launch: '', // wns
+                                duration: '', // wns
+                                consolidationKey: 'my notification', // ADM
+                            };
+                           const registrationIds = [];
+                           for (i = 0; i !== result.length ; i++) {
+                                if (result[i].device_id !== null && result[i].device_id !== "")
+                                    registrationIds.push(result[i].device_id);
+                            }
+                            push.send(registrationIds, data, (err, result) => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log(result);
+                                }
+                            });
+                        }
+                        client.close();
+                    });
+
                     res.send(JSON.stringify({"state": "success"}));
 
                 } else {
